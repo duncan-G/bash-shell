2e6b45f891feb7920d7491839a116f8810c3ab57
Duncan <duncangichimu@gmail.com>
2018-11-06 11:38:45 -0800

    add pwd command

--- bash.js
+++ bash.js
@@ -1,1 +1,13 @@
+const bash = {
+  pwd: process.cwd()
+};
 
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', data => {
+  process.stdout.write(data);
+  const cmd = data.toString().trim();
+
+  process.stdout.write(bash[cmd] || '');
+  process.stdout.write('\nprompt > ');
+});
