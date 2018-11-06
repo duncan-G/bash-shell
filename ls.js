8cb10376671683f5a3a048e27971cd21f1f75399
Duncan <duncangichimu@gmail.com>
2018-11-06 13:31:01 -0800

    add ls.js

---
+++ ls.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+function ls(args, options, logger) {
+  if (args.length === 0) {
+    args.push('./');
+  }
+
+  fs.readdir(...args, options, (err, files) => {
+    if (err) {
+      logger(err);
+    } else {
+      logger(files.join(','));
+    }
+  });
+}
+
+module.exports = ls;
